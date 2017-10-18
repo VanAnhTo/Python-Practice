f71c66fd680db1e41f2d40159897b64e6fe9e5d3
Anh To Thi Van <vananhto.bg@gmail.com>
2017-10-18 17:35:12 +0700

    Example_myPet

---
+++ Chapter5/myPets.py
@@ -0,0 +1,8 @@
+myPet = ['Tom','Mickey', 'Cruise']
+
+print('Enter cat name: ')
+catName = input()
+if catName in myPet:
+    print (catName + ' is my pet.')
+else:
+    print(catName + ' is not my pet')
