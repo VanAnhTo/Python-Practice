19a1f0bbce8f7f46143d64815d3c6d43ec7144ba
Anh To Thi Van <vananhto.bg@gmail.com>
2017-12-05 07:42:28 +0700

    Theory chapter 8
    from another computer

--- Chapter_8/ReadandWriteFiles.py
+++ Chapter_8/ReadandWriteFiles.py
@@ -2,6 +2,74 @@ import os
 import shelve
 import pprint
 import myCats
+
+#------------
+#Backslash on Windows
+'''
+myFiles = ['accounts.txt', 'details.csv', 'invite.docx']
+for filename in myFiles:
+    print(os.path.join('D:\\AnhTo\\git\\Pycharm\\Draft', filename))
+#D:\AnhTo\git\Pycharm\Draft\accounts.txt
+#D:\AnhTo\git\Pycharm\Draft\details.csv
+#D:\AnhTo\git\Pycharm\Draft\invite.docx
+'''
+
+#The Current Working Directory
+'''
+print( os.getcwd())
+#D:\AnhTo\git\Pycharm\Automated_boring_stuff\Chapter_7
+
+os.chdir('C:\\Windows\\System32')
+print(os.getcwd())
+#C:\Windows\System32
+'''
+
+#Handling Absolute and Relative Paths
+# os.path.abspath(path) : Return a string of the absolute path ò the argument
+# os.path.isabs(path): return True if the argument is an absolute path.
+# os.path.relpath(path, start): return a string of a relative path from the start path to path
+'''
+print(os.path.abspath('.'))
+#D:\AnhTo\git\Pycharm\Automated_boring_stuff\Chapter_7
+
+print(os.path.abspath('.\\Chapter_7'))
+#D:\AnhTo\git\Pycharm\Automated_boring_stuff\Chapter_7\Chapter_7
+
+print(os.path.isabs('.'))
+#False
+
+print(os.path.isabs(os.path.abspath('.')))
+#True
+
+path = 'C:\\Windows\\System32\\calc.exe'
+print(os.path.basename(path))
+#calc.exe
+
+print(os.path.dirname(path))
+#C:\Windows\System32
+
+print(os.path.split(path))
+#('C:\\Windows\\System32', 'calc.exe')
+
+print((os.path.dirname(path), os.path.basename(path)))
+#('C:\\Windows\\System32', 'calc.exe')
+
+print(path.split(os.path.sep))
+#['C:', 'Windows', 'System32', 'calc.exe']
+'''
+
+#Finding File Sizes and Folder Contents
+# os.path.getsize(path): return the size in bytes of the file in the path argument
+# os.listdir(path): return a list of filename strings for each file in the path argument
+'''
+print(os.path.getsize('C:\\Windows\\System32\\calc.exe'))
+#918528
+
+print(os.listdir('D:\\AnhTo\\Git\\Project'))
+#['Bootstrap', 'Eclipse', 'IBM-master', 'IBM-master.zip', 'XcartTestNG', 'XcartTestNG.rar']
+'''
+#----------------
+
 #Opening Files with the open() Function
 '''
 helloFile = open('E:\download\Skype received files\hello.txt')
