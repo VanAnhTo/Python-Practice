83e32e1144e03c926a5624aab9a2f0d8e78dff8c
Anh To Thi Van <vananhto.bg@gmail.com>
2017-12-07 14:55:47 +0700

    MadLibs project chapter 8

---
+++ Chapter_8/MadLibs.py
@@ -0,0 +1,37 @@
+import re
+
+madFile = open('MadLibTestFile.txt')
+madContent = madFile.read()
+print(madContent)
+madFile.close()
+regex = re.compile(r'ADJECTIVE|NOUN|VERB')
+
+mad = regex.findall(madContent)
+print(mad)
+
+madFile = open('MadLibTestFile.txt','w')
+for i in range (len(mad)):
+
+    if mad[i] == 'ADJECTIVE':
+        adj = input('Enter an adjective: ')
+        reAdj = re.compile(r'ADJECTIVE')
+        madContent = reAdj.sub(adj,madContent)
+
+    if mad[i] == 'NOUN':
+
+        noun = input('Enter a noun: ')
+        reNoun = re.compile(r'NOUN')
+        madContent = reNoun.sub(noun, madContent)
+
+    if mad[i] == 'VERB':
+
+        verb = input('Enter a verb: ')
+        reVerb = re.compile(r'VERB')
+        madContent = reVerb.sub(verb, madContent)
+
+
+madFile.write(madContent)
+madFile.close()
+madFile = open('MadLibTestFile.txt')
+madContent = madFile.read()
+print(madContent)
