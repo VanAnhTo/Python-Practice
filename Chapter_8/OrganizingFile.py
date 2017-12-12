daf5d5c8f909ae1e7e3b53066003185c2c8e7e02
Anh To Thi Van <vananhto.bg@gmail.com>
2017-12-12 17:49:35 +0700

    Chapter 9_ Organizing File

--- Chapter_8/OrganizingFile.py
+++ Chapter_8/OrganizingFile.py
@@ -40,3 +40,56 @@ for filename in os.listdir():
 
 #Safe Deletes with the send2trash Module
 
+#send2trash() function can only send files to the recycle bin
+'''
+baconFile = open('TestChapter9\\bacon.txt', 'a') # creates the file
+baconFile.write('Bacon is not a vegetable.') #write the content
+baconFile.close()
+send2trash.send2trash('bacon.txt') 
+'''
+
+#Walking a Directory Tree
+#os.walk()function will return three values on each iteration through the loop:
+#1. A string of the current folder’s name
+#2. A list of strings of the folders in the current folder
+#3. A list of strings of the files in the current folder
+folderPath = "D:\\AnhTo\\Draft\\"
+'''
+for folderName, subfolders, filenames in os.walk(folderPath):
+    print('The current folder is ' + folderName)
+    for subfolder in subfolders:
+        print('SUBFOLDER OF ' + folderName + ': ' + subfolder)
+    for filename in filenames:
+        print('FILE INSIDE ' + folderName + ': ' + filename)
+    print('')
+'''
+
+#Compressing Files with the zipfile Module
+#namelist(): returns a list of strings for all the files and folders contained in the ZIP file.
+'''
+os.chdir(folderPath)
+exampleZip = zipfile.ZipFile('example.zip')
+print(exampleZip.namelist())
+spamInfo = exampleZip.getinfo('Cassiophia.txt')
+print(spamInfo.file_size)
+print(spamInfo.compress_size)
+print('Compressed file is %sx smaller!' % (round(spamInfo.file_size / spamInfo.compress_size, 2)))
+exampleZip.close()
+'''
+
+#Extracting from ZIP Files
+#extractall(): extracts all the files and folders from a ZIP file into the current working directory.
+#extract() : method for ZipFile objects will extract a single file from the ZIP file.
+#The value that extract() returns is the absolute path to which the file was extracted.
+'''
+os.chdir(folderPath) # move to the folder with example.zip
+exampleZip = zipfile.ZipFile('example.zip')
+
+exampleZip.extractall(folderPath+ 'AfterExtractFolder')
+
+exampleZip.extract('Cassiophia.txt')
+exampleZip.extract('Cassiophia.txt', folderPath+ 'AfterExtractFolder')
+exampleZip.close()
+'''
+
+#Creating and Adding to ZIP Files
