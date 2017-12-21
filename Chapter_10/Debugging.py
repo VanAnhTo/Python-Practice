048c75f29b08f69dd44434f2aac07a5b1ecef4b0
Anh To Thi Van <vananhto.bg@gmail.com>
2017-12-21 08:23:56 +0700

    Chapter 10 completed

--- Chapter_10/Debugging.py
+++ Chapter_10/Debugging.py
@@ -64,5 +64,43 @@ switchLights(market_2nd)
 #Logging
 #Using the logging Module
 
+#Don’t Debug with print()
+
+#Logging Levels
+#logging.debug(): The lowest level. Used for small details. Usually you care about these messages only when diagnosing problems.
+#logging.info(): Used to record information on general events in your program
+                # or confirm that things are working at their point in the program.
+#logging.warning(): Used to indicate a potential problem that doesn’t prevent
+                # the program from working but might do so in the future.
+#logging.error(): Used to record an error that caused the program to fail to do something.
+#logging.critical(): The highest level.
+                # Used to indicate a fatal error that has caused or is about to cause the program to stop running entirely.
+'''
+logging.basicConfig(level=logging.DEBUG, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Some debugging details.')
+logging.info('The logging module is working.')
+logging.warning('An error message is about to be logged.')
+logging.error('An error has occurred.')
+logging.critical('The program is unable to recover!')
+'''
+
+#***The benefit of logging levels is that you can change what priority of logging message you want to see
+
+#Disabling Logging
+#**logging.disable() a logging level
+#So if you want to disable logging entirely, just add logging.disable(logging.CRITICAL) to your program
+'''
+logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
+logging.critical('Critical error! Critical error!')
+logging.disable(logging.CRITICAL)
+logging.critical('Critical error! Critical error!')
+logging.error('Error! Error!')
+'''
+#***logging.disable() will disable all messages after it
+
+#Logging to a File
+
+logging.basicConfig(filename='myProgramLog.txt', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 
