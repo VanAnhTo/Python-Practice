3975444ef23a0bece03360167888d1b12d70e6a1
VanAnhTo <vananhto.bg@gmail.com>
2018-01-08 23:05:25 +0700

    Chapter 14

--- Chapter_14/quickWeather.py
+++ Chapter_14/quickWeather.py
@@ -4,11 +4,26 @@
 import json, requests, sys
 
 # Compute location from command line arguments.
+'''
 if len(sys.argv) < 2:
     print('Usage: quickWeather.py location')
     sys.exit()
 location = ' '.join(sys.argv[1:])
-
-url ='http://api.openweathermap.org/data/2.5/forecast/daily?q=%s&cnt=3' % (location)
+'''
+location = 'Hanoi'
+url ='http://api.openweathermap.org/data/2.5/forecast?q=%s&appid=c78b28600feeb7d1e2b063c0af2d1da6&cnt=3' % (location)
 response = requests.get(url)
 response.raise_for_status()
+
+# Load JSON data into a Python variable.
+weatherData = json.loads(response.text)
+w = weatherData['list']
+print('Current weather in %s:' % (location))
+print(w[0]['weather'][0]['main'], '-', w[0]['weather'][0]['description'])
+print()
+print('Tomorrow:')
+print(w[1]['weather'][0]['main'], '-', w[1]['weather'][0]['description'])
+print()
+print('Day after tomorrow:')
+print(w[2]['weather'][0]['main'], '-', w[2]['weather'][0]['description'])
+
