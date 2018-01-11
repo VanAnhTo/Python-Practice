ff2844a3300c9d482dfd47f77a1a45360e601611
Anh To Thi Van <vananhto.bg@gmail.com>
2018-01-11 08:12:08 +0700

    Write from excel to text files

--- Chapter_12/writeToText.py
+++ Chapter_12/writeToText.py
@@ -6,7 +6,20 @@ from numpy.distutils.system_info import numarray_info
 from openpyxl.styles import Font
 from openpyxl.utils import get_column_letter, column_index_from_string
 
-excelFile = '1.xlsx'
+
+excelFile = 'readLines.xlsx'
 wb = openpyxl.load_workbook(excelFile)
 sheet = wb.get_sheet_by_name('Sheet')
 
+for j in range (1, sheet.max_column+1):
+
+    textFile = open('Text_%s.txt'%j, 'w')
+
+    for i in range (1, sheet.max_row+1):
+        val = sheet.cell(row=i, column=j).value
+        if val == None:
+            continue
+        print(j,i,val)
+        textFile.write(val)
+
+    print('-----------')
