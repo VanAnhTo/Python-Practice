4453cc8b550405ea4a1687496d1c3f3e25397a8a
Anh To Thi Van <vananhto.bg@gmail.com>
2018-02-08 17:59:38 +0700

    Update sendEmailTextMessage.py

--- Chapter_16/sendEmailTextMessage.py
+++ Chapter_16/sendEmailTextMessage.py
@@ -37,3 +37,43 @@ imapObj = imapclient.IMAPClient('imap.gmail.com', ssl=True)
 print(imapObj.login(' anhanh29bg@gmail.com ', '******'))
 pprint.pprint(imapObj.list_folders())
 imapObj.select_folder('INBOX', readonly=True)
+
+# Searching for Email
+
+# Selecting a Folder
+pprint.pprint(imapObj.list_folders())
+
+imapObj.select_folder('INBOX', readonly=True)
+
+# Performing the Search
+
+print(imapObj.search(['ALL']))
+imapObj.search(['ON 05-Feb-2018'])
+
+imapObj.search(['SINCE 05-Jan-2017', 'BEFORE 05-Feb-2018', 'UNSEEN'])
+imapObj.search(['SINCE 05-Jan-2017', 'FROM alice@example.com'])
+imapObj.search(['SINCE 05-Jan-2017', 'NOT FROM alice@example.com'])
+imapObj.search(['OR FROM alice@example.com FROM bob@example.com'])
+imapObj.search(['FROM alice@example.com', 'FROM bob@example.com'])
+
+#****The search() method doesn’t return the emails themselves
+# but rather unique IDs (UIDs) for the emails, as integer values
+
+#Size Limits
+#***This limit is in place to prevent your Python programs from eating up too much memory
+# the default size limit is often too small.You can change this limit
+# from 10,000 bytes to 10,000,000 bytes by running this code:
+
+imaplib._MAXLINE = 10000000
+
+#If you are logging in to a Gmail account,
+# pass the search terms to the gmail_search() method instead of the search() method
+UIDs = imapObj.gmail_search('meaning of life')
+
+#Fetching an Email and Marking It As Read
+#you have a list of UIDs, you can call the IMAPClient object’s fetch() method to get the actual email content
+
+rawMessages = imapObj.fetch(UIDs, ['BODY[]'])
+pprint.pprint(rawMessages)
+
+#Getting Email Addresses from a Raw Message
