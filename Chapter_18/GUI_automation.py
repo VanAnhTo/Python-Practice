9b4295571c61e23848d7fda68df11dd2a602d31d
VanAnhTo <vananhto.bg@gmail.com>
2018-02-25 21:35:16 +0700

    Chapter 18

---
+++ Chapter_18/GUI_automation.py
@@ -0,0 +1,15 @@
+#Installing the pyautogui Module
+#pyautogui
+
+## Staying on Track
+
+#Pauses and Fail-Safes
+import pyautogui
+
+pyautogui.PAUSE = 1
+pyautogui.FAILSAFE = True
+
+#Controlling Mouse Movement
+
+print(pyautogui.size())
+
