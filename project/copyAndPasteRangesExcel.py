8f0e980a805da4562664a54052e5f7ea1d4c0f23
VanAnhTo <vananhto.bg@gmail.com>
2018-05-23 22:05:02 +0700

    copy style of a cell

--- project/copyAndPasteRangesExcel.py
+++ project/copyAndPasteRangesExcel.py
@@ -54,16 +54,3 @@ def createData():
     print("Range copied and pasted!")
 
 createData()
-
-def copyRangeWithStyle(startCol, startRow, endCol, endRow, sheet):
-    rangeSelected = []
-    # Loops through selected Rows
-    for i in range(startRow, endRow + 1, 1):
-        # Appends the row to a RowSelected list
-        rowSelected = []
-        for j in range(startCol, endCol + 1, 1):
-            rowSelected.append(sheet.cell(row=i, column=j).value)
-        # Adds the RowSelected List and nests inside the rangeSelected
-        rangeSelected.append(rowSelected)
-
-    return rangeSelected
--- project/copyStyle.py
+++ project/copyStyle.py
@@ -1,24 +1,52 @@
 import openpyxl
+#from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
+from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font, Color, colors
+from copy import copy
 
 # File to be copied
 wb = openpyxl.load_workbook("File_Copy.xlsx")  # Add file name
 sheet = wb["Score"] # Add Sheet name
 
 
-new_sheet = wb.create_sheet('Sheet3')
+
+a  = sheet['B1']
+b = sheet['G3']
+
+#print(b.font, b.border)
+
+a._style = copy(b._style)
+
+# ft = Font(color=colors.RED, name='Arial', size=18)
+# a.font = ft
+# fl = PatternFill(fill_type= 'solid' , start_color='FFFFCC')
+# a.fill = fl
+# bor = Border(
+#     left=Side(border_style='thin',color='000055'),
+# right=Side(border_style='thin',color='000055'),
+# top=Side(border_style='thin',color='000055'),
+# bottom=Side(border_style='thin',color='000055'),
+# diagonal=Side(border_style='thin',color='000055'),
+# diagonal_direction=0,
+# outline=Side(border_style='thin',color='000055'),
+# vertical=Side(border_style='thin',color='000055'),
+# horizontal=Side(border_style='thin',color='000055'))
+# a.border = bor
 
 
+wb.save("File_Copy.xlsx")
+
+'''
 for row in sheet.rows:
     for cell in row:
         new_cell = new_sheet.cell(row=3,
-                   col=2, value= sheet['B3'].value)
+                                  column=2, value=cell.value)
         if cell.has_style:
-            new_cell.font = cell.font
-            new_cell.border = cell.border
-            new_cell.fill = cell.fill
-            new_cell.number_format = cell.number_format
-            new_cell.protection = cell.protection
-            new_cell.alignment = cell.alignment
-
+            cell.Font = new_cell.Font
+            # new_cell.Border = cell.Border
+            # new_cell.Fill = cell.Fill
+            # new_cell.Number_format = cell.Number_format
+            # new_cell.Protection = cell.Protection
+            # new_cell.Alignment = cell.Alignment
+'''
 
 
