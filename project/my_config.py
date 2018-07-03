26529811754f855e4edff4de2e95bbfab62d6f46
Anh To Thi Van <vananhto.bg@gmail.com>
2018-07-03 16:58:56 +0700

    Completed (configure file)

---
+++ project/my_config.py
@@ -0,0 +1,26 @@
+#Configure file directory
+#---------
+#Not change the comment file template
+#Hide row, colume still be read, so be noticed!
+
+scoring_file = "Scoring_file.xlsx"
+sheet_score_name = 'Export'
+
+comment_file = "CommentToCodeBeamer.xlsx"
+sheet_comment_name = 'Comment'
+
+firefox_geckodriver = r'D:\AnhTo\Setup\Wed_driver\geckodriver.exe'
+
+code_beamer_link = 'http://***********/cb/projects/browse.spr'
+
+username = 'username'
+password = 'password'
+
+srs_link = 'http://***********/cb/issue/'
+
+autoIT_copy_data_exe = "open_copy_excelfile.exe"
+
+autoIT_close_excel = "close_excel_file.exe"
+
+#with autoIT file, if change the path of the tempalate comment file change,
+#contact: Van Anh To
